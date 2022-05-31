0bd88b3f48731b18898d8c08425cb9c41c14a3a5
James Benerofe <Jamessbenerofe23@gmail.com>
2022-05-30 17:32:02 -0700

    finished

--- src/script.js
+++ src/script.js
@@ -1,8 +1,1 @@
 `strict`
-
-const content = document.getElementById("content");
-const navbardiv = document.createElement("ul");
-h1.textContent = "BAKA";
-paragraph.textContent = "This pizza shop is so very good and will help clense all of your cravings. This is the most decident peice of pizza that will ever touch your tongue."
-
-content.append(paragraph, h1);
