4951ec43f947a8e38fd11f916aec9bb5d2f081b1
James Benerofe <Jamessbenerofe23@gmail.com>
2022-05-30 17:20:26 -0700

    started basics

---
+++ main.js
@@ -0,0 +1,1 @@
+(()=>{const e=document.getElementById("content");document.createElement("ul"),h1.textContent="BAKA",paragraph.textContent="This pizza shop is so very good and will help clense all of your cravings. This is the most decident peice of pizza that will ever touch your tongue.",e.append(paragraph,h1)})();
